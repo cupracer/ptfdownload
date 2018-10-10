40b3db09963fd6e50ff0c17c7642e8ffe3e56ca9
Thomas Schulte <thomas.schulte@suse.com>
2018-10-10 14:19:40 +0200

    add module test script

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from PTFload import doPtfDownload
+
+outputdir = './'
+url = ''
+username = ''
+password = ''
+includeOptional = False
+verbose = True
+
+doPtfDownload(outputdir, url, username, password, includeOptional, verbose)
+
