7f9949d7f23642d0759de5a1b7ccc077a9759c87
Thomas Schulte <thomas.schulte@suse.com>
2019-07-04 09:41:45 +0200

    use correct project name

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2018 Thomas Schulte.  All rights reserved.
 #
-# This file is part of ptfdownload.
+# This file is part of cupracer/ptfdownload.
 #
 # ptfdownload is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
